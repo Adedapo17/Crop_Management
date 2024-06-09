ad8aa5707e8debde519469e9b5a78fb10a58fb2a
Olaifa Adedapo <74598417+Adedapo17@users.noreply.github.com>
2024-06-09 17:35:58 +0100

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -11,7 +11,8 @@ const handler = NextAuth({
             clientId:process.env.GOOGLE_ID,
             clientSecret:process.env.GOOGLE_SECRET
         }),secret
-    ]
+    ],
+     secret: process.env.NEXTAUTH_SECRET,
 })
 
 export {handler as GET, handler as POST}
